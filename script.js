20d58b5051b3fe3708815f1eeb6dc2af7db38c70
timleung0224 <timleung0224@gmail.com>
2019-02-01 16:04:06 +0800

    check player1 win

--- script.js
+++ script.js
@@ -1,83 +1,210 @@
-var pl1pt = "";
-//var win = "0"&&"1"&&"2"+"3"&&"4"&&"5";
-//"34,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6
-var win1 = ["0","1","2"];
-var win2 = "3"&&"4"&&"5";
-var win3 = "6"&&"7"&&"8";
-var win4 = "0"&&"3"&&"6";
-var win5 = "1"&&"4"&&"7";
-var win6 = "2"&&"5"&&"8";
-var win7 = "0"&&"4"&&"8";
-var win8 = "2"&&"4"&&"6";
+var player1point = "0";
+var player2point = "1";
 var turn = (0);
-var win = false;
-var i = "";
-//var check = 0;
-var pl2pt = "";
+var board = [
+   ['','',''],
+   ['','',''],
+   ['','','']  
+];
+function player1EndGame() {
+   document.getElementById("fin").innerHTML = "<h2>Player1 Wins</h2>";
+   for(var i = 0; i < gamepad.length; i++) {
+       gamepad[i].disabled = true;
+   }
+}
+function checkPlayer1Win() {
+   for (var i = 0 ; i < 3; i++){
+      if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ""){
+         var a = player1EndGame();       
+      } 
+      if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ""){
+         var a = player1EndGame();       
+      }      
+   }  
+   if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ""){
+      var a = player1EndGame();
+   }
+   if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != ""){
+      var a = player1EndGame();
+   }
+}   
+   //for (var j = 0 ; j < board.length; j++) {  
+      //board[x][y] = board[y][x];    
+      //var a = [x];
+      //var b = [y];
+      //[y,x] = temp;
+      //board[b][a] = player1point;
+      //console.log(board[j]);
+      //if (board[j].every(element =>element === player1point)){
+        // document.getElementById("fin").innerHTML = "<h2>Player1 Wins</h2>";
+         //var a = endGame();
+      //}  
+  //}   
+//}
+
 var gamepad = document.getElementsByClassName("game-pad");
+function onBtnClick([x,y],this_id){
+   var a = this_id;
+   document.getElementById(a).disabled = true;    
+   
 
-function onBtnClick(this_id){
-
-    var a = (this_id);
-    document.getElementById(a).disabled = true;
-    //alert(win1)
-      
    if (turn%2 == 0){
-       document.getElementById(a).innerHTML = "O";
-       pl1pt += a;
+      document.getElementById(a).innerHTML = "O";
+      
+      board[x][y] = player1point; 
+      var winner = checkPlayer1Win()
+   } 
+   else 
+   {
+      document.getElementById(a).innerHTML = "X";
+      board[x][y] = player2point;
        
-       //switch(pl1pt){
-          // case "0"+"1":
-           //win = true;
-          // alert(pl1pt);
-           //break;
-           //default :
-           //win = false;
-           
+   }
+   turn ++;
+   if (turn > 8 ){
+      document.getElementById("fin").innerHTML = "<h2>Tie</h2>";
+   }
+   //var winner = checkWin();
+   
+      //return player1point
+   //}
+
+   /*if (turn%2 == 0){
+      player1point.innerHTML = "O";
+      board[x][y] = player1point;  
+      for (var i = 0 ; i < board.length; i++) {
+         if (board[i].every(element =>element === player1point)){
+            document.getElementById("fin").innerHTML = "<h2>Player1 Wins</h2>";
+            var a = endGame();
+         }  
+      }
+      for (var j = 0 ; j < board.length; j++) {      
+         var a = [x];
+         var b = [y];
+         board[b][a] = player1point;
+         console.log(board[j]);
+         if (board[j].every(element =>element === player1point)){
+            document.getElementById("fin").innerHTML = "<h2>Player1 Wins</h2>";
+            var a = endGame();
+         }  
+      }   
+   }
+   
+  
+   //for (var i = 0 ; i < board.length; i++) {
+      //if (board[i].every(check)){
+     // board[x][y] = player1point;
+      //console.log(board[i]);
+      //}    
+   //}
+   //for (var j = 0 ; j < board.length; j++) {   
+      //board[x][y] = player1point;   
+      //var temp = [x,y];
+      //[x,y] = [y,x];
+      //[y,x] = temp;
+      //console.log([x,y])
+      //if (board[j].every(check)){
+          
+            //console.log(board[j]);
+     // }
+         
+         
+      
+      
+   
+      // var checking = i.every( check );
+      //var checking = [3,4,5].every( check );
+      //if (check = true){
+         
+      //}
+      
+      //for (j = 0; j < 2; j++) {
+      //}
+   //}
+    //alert(win1)
+            // return (board[i].every((item) => item === "sample"));
+         
+            //var element = board[i].every(element =>element === player1point);
+
+         //console.log(element);      
+         //console.log(board);
+            
+   
+      //for (var j = 0 ; j < board.length; j++) {   
+         //board[x][y] = player1point;   
+         //var temp = [x,y];
+         //[x,y] = [y,x];
+         //[y,x] = temp;
+         //console.log([x,y])
+         //if (board[j].every(check)){            
+            //document.getElementById("fin").innerHTML = "<h2>Player1 Wins</h2>";
+            //break
+        // }
+            
+            
+         
+         
+      //}
+       
+      
            
 
-       //}
+   
       // if (win = true){
          //  alert(pl1pt)
       // }
        
-      if (pl1pt.includes("0")&&pl1pt.includes("1")&&pl1pt.includes("2")||pl1pt.includes("3")&&pl1pt.includes("4")&&pl1pt.includes("5")||pl1pt.includes("6")&&pl1pt.includes("7")&&pl1pt.includes("8")||pl1pt.includes("0")&&pl1pt.includes("3")&&pl1pt.includes("6")||pl1pt.includes("1")&&pl1pt.includes("4")&&pl1pt.includes("7")||pl1pt.includes("2")&&pl1pt.includes("5")&&pl1pt.includes("8")||pl1pt.includes("0")&&pl1pt.includes("4")&&pl1pt.includes("8")||pl1pt.includes("2")&&pl1pt.includes("4")&&pl1pt.includes("6")){
-         alert("player 1 win");
-         document.getElementById("fin1").style.zIndex = "2";
-         for(var i = 0; i < gamepad.length; i++) {
-            gamepad[i].disabled = true;
-         }
+      //if (player1point.includes("0")&&player1point.includes("1")&&player1point.includes("2")||player1point.includes("3")&&player1point.includes("4")&&player1point.includes("5")||player1point.includes("6")&&player1point.includes("7")&&player1point.includes("8")||player1point.includes("0")&&player1point.includes("3")&&player1point.includes("6")||player1point.includes("1")&&player1point.includes("4")&&player1point.includes("7")||player1point.includes("2")&&player1point.includes("5")&&player1point.includes("8")||player1point.includes("0")&&player1point.includes("4")&&player1point.includes("8")||player1point.includes("2")&&player1point.includes("4")&&player1point.includes("6")){
+         //alert("player 1 win");
+         //document.getElementById("fin").innerHTML = "<h2>Player1 Wins</h2>";
+         //for(var i = 0; i < gamepad.length; i++) {
+            //gamepad[i].disabled = true;
+         //}
          //document.getElementByClass("game-pad").disabled = true;
-      }
+      //}
 
       //if (pl1pt.includes(win1)||pl1pt.includes(win2)||pl1pt.includes(win3)||pl1pt.includes(win4)||pl1pt.includes(win5)||pl1pt.includes(win6)||pl1pt.includes(win7)||pl1pt.includes(win8)){
         
       //if (win1 == pl1pt ){  
       //alert(win1);
       //}                    
-      }
+      //}
    else {
       document.getElementById(a).innerHTML = "X";
-      pl2pt += [a];
-      if (pl2pt.includes("0")&&pl2pt.includes("1")&&pl2pt.includes("2")||pl2pt.includes("3")&&pl2pt.includes("4")&&pl2pt.includes("5")||pl2pt.includes("6")&&pl2pt.includes("7")&&pl2pt.includes("8")||pl2pt.includes("0")&&pl2pt.includes("3")&&pl2pt.includes("6")||pl2pt.includes("1")&&pl2pt.includes("4")&&pl2pt.includes("7")||pl2pt.includes("2")&&pl2pt.includes("5")&&pl2pt.includes("8")||pl2pt.includes("0")&&pl2pt.includes("4")&&pl2pt.includes("8")||pl2pt.includes("2")&&pl2pt.includes("4")&&pl2pt.includes("6")){
-         alert("player 1 win");
-         document.getElementById("fin2").style.zIndex = "2";
-         for(var i = 0; i < gamepad.length; i++) {
-             gamepad[i].disabled = true;
-         }
+      board[x][y] = player2point;
+      for (var i = 0 ; i < board.length; i++) {
+         if (board[i].every(element =>element === player2point)){
+            document.getElementById("fin").innerHTML = "<h2>Player2 Wins</h2>";
+            var a = endGame();
+         }    
       }
+      /*for (var j = 0 ; j < board.length; j++) {   
+         board[x][y] = player2point;   
+         var temp = [x,y];
+         [x,y] = [y,x];
+         [y,x] = temp;
+         //console.log([x,y])
+         if (board[j].every(check)){
+            document.getElementById("fin").innerHTML = "<h2>Player2 Wins</h2>"; 
+              // console.log(board[j]);
+         }
+      }   
+      //player2point += [a];
+      //if (player2point.includes("0")&&player2point.includes("1")&&player2point.includes("2")||player2point.includes("3")&&player2point.includes("4")&&player2point.includes("5")||player2point.includes("6")&&player2point.includes("7")&&player2point.includes("8")||player2point.includes("0")&&player2point.includes("3")&&player2point.includes("6")||player2point.includes("1")&&player2point.includes("4")&&player2point.includes("7")||player2point.includes("2")&&player2point.includes("5")&&player2point.includes("8")||player2point.includes("0")&&player2point.includes("4")&&player2point.includes("8")||player2point.includes("2")&&player2point.includes("4")&&player2point.includes("6")){
+         //document.getElementById("fin").innerHTML = "<h2>Player2 Wins</h2>";
+         //for(var i = 0; i < gamepad.length; i++) {
+             //gamepad[i].disabled = true;
+        // }
+      //}*/
 
-   }
+   //}
     //document.getElementById(a).disabled = true;
        // check = 0 ; check < pl1pt.length; check++; 
 
        //if( pl1pt.substr(check) == win1.substr(check) ){check++;}
        // alert(check);
     //}  
-    turn ++;
-    if (turn > 8 ){
-       document.getElementById("fin3").style.zIndex = "2"; 
-    }
+   
 }
 function onRestBtnClick(){
     location.reload();
